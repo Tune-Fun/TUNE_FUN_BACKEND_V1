4944654ffadba07332e41d469c693a20be4adc31
habin <hbkim@cyrexpay.com>
2024-02-23 15:40:28 +0900

    configuration(profile): not_allow_test
    NotAllowTest, OnlyDevelopmentConfiguration 어노테이션 추가

--- src/main/java/com/tune_fun/v1/common/aspect/AwsXRayAspect.java
+++ src/main/java/com/tune_fun/v1/common/aspect/AwsXRayAspect.java
@@ -2,13 +2,16 @@ package com.tune_fun.v1.common.aspect;
 
 import com.amazonaws.xray.entities.Subsegment;
 import com.amazonaws.xray.spring.aop.BaseAbstractXRayInterceptor;
+import com.tune_fun.v1.common.config.annotation.NotAllowTest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@NotAllowTest
 @Aspect
 @Component
 public class AwsXRayAspect extends BaseAbstractXRayInterceptor {
