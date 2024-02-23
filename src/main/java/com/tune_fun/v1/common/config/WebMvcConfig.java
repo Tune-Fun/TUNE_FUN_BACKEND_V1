142e745dc0236e334ab5675665c2e1cc5969367a
habin <hbkim@cyrexpay.com>
2024-02-23 14:54:46 +0900

    configuration(monitoring): aws_xray
    AWS Xray Aspect, Inspector 추가

--- src/main/java/com/tune_fun/v1/common/config/WebMvcConfig.java
+++ src/main/java/com/tune_fun/v1/common/config/WebMvcConfig.java
@@ -1,8 +1,13 @@
 package com.tune_fun.v1.common.config;
 
+import com.amazonaws.xray.jakarta.servlet.AWSXRayServletFilter;
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static com.amazonaws.xray.strategy.jakarta.SegmentNamingStrategy.dynamic;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -13,6 +18,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css");
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
         registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+    }
+
+    @Bean
+    public Filter TracingFilter() {
+        return new AWSXRayServletFilter(dynamic("Scorekeep"));
     }
 
 }
