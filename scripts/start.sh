931eb1a57be423bffe94a78af1d611914416176a
habin <hbkim@cyrexpay.com>
2024-03-08 09:41:09 +0900

    config(workflow): cd
    start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,7 @@ JAR_FILE=$PROJECT_ROOT/build/libs/tune_fun-0.0.1-SNAPSHOT.jar
 APP_LOG=$PROJECT_ROOT/app.log
 ERROR_LOG=$PROJECT_ROOT/error.log
 DEPLOY_LOG=$PROJECT_ROOT/deploy.log
+JAVA_EXECUTABLE=/usr/local/jdk-21/bin/java
 
 TIME_NOW=$(date +%c)
 
@@ -14,7 +15,7 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE --spring.profiles.active=dev > $APP_LOG 2> $ERROR_LOG &
+nohup $JAVA_EXECUTABLE -jar $JAR_FILE --spring.profiles.active=dev > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
