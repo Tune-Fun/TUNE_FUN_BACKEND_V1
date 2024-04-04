e04d68e8fe17e68c97a7535e8c23c986715c0e28
habin <danny9643@naver.com>
2024-04-04 23:37:31 +0900

    update start.sh, stop.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_ROOT="/home/habin/action"
-JAR_FILE=$PROJECT_ROOT/build/libs/tune_fun-0.0.1-SNAPSHOT.jar
+JAR_FILE=$PROJECT_ROOT/tune_fun-0.0.1-SNAPSHOT.jar
 
 APP_LOG=$PROJECT_ROOT/app.log
 ERROR_LOG=$PROJECT_ROOT/error.log
@@ -12,7 +12,7 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/build/libs/tune_fun-0.0.1-SNAPSHOT.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
