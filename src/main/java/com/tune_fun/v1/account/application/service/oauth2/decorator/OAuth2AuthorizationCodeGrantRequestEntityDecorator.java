7a7481ca71b9220a1148caac6eccece6d4b6fe50
habin <danny9643@naver.com>
2024-04-06 21:23:11 +0900

    feat(oauth2) AccessToken
    KmsEncryptionHelper ㅂ

--- src/main/java/com/tune_fun/v1/account/application/service/oauth2/decorator/OAuth2AuthorizationCodeGrantRequestEntityDecorator.java
+++ src/main/java/com/tune_fun/v1/account/application/service/oauth2/decorator/OAuth2AuthorizationCodeGrantRequestEntityDecorator.java
@@ -1,12 +1,10 @@
 package com.tune_fun.v1.account.application.service.oauth2.decorator;
 
 import com.tune_fun.v1.common.property.AppleProperty;
+import com.tune_fun.v1.common.util.EncryptUtil;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.http.RequestEntity;
@@ -22,6 +20,7 @@ import java.time.Instant;
 import java.util.Date;
 
 import static com.tune_fun.v1.account.domain.state.oauth2.OAuth2Provider.APPLE;
+import static org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.CLIENT_SECRET;
 
 @Slf4j
 @Component
@@ -52,18 +51,21 @@ public class OAuth2AuthorizationCodeGrantRequestEntityDecorator extends OAuth2Au
 
         String clientSecret;
         try {
-             clientSecret = createAppleClientSecret();
+            clientSecret = createAppleClientSecret();
         } catch (IOException e) {
             throw new IllegalStateException("Error while creating Apple client secret.");
         }
 
-        params.set("client_secret", clientSecret);
+        params.set(CLIENT_SECRET, clientSecret);
 
         return new RequestEntity<>(params, entity.getHeaders(), entity.getMethod(), entity.getUrl());
     }
 
     private String createAppleClientSecret() throws IOException {
         Instant expirationInstant = Instant.now().plusSeconds(30 * 24 * 60 * 60); // 30 days in seconds
+        final StringReader appleOAuth2KeyReader = new StringReader(appleProperty.oauth2Key());
+        final PrivateKey appleOAuth2PrivateKey = EncryptUtil.parsePemAndGetPrivateKey(appleOAuth2KeyReader);
+
 
         return Jwts.builder()
                 .header().add("kid", appleProperty.keyId())
@@ -74,23 +76,8 @@ public class OAuth2AuthorizationCodeGrantRequestEntityDecorator extends OAuth2Au
                 .audience().add("https://appleid.apple.com")
                 .and()
                 .subject(oAuth2ClientProperties.getRegistration().get(APPLE.getRegistrationId()).getClientId())
-                .signWith(getPrivateKey(), Jwts.SIG.ES256)
+                .signWith(appleOAuth2PrivateKey, Jwts.SIG.ES256)
                 .compact();
-    }
-
-
-    /**
-     * Retrieves the private key used for signing from a PEM-formatted string.
-     *
-     * @return The private key.
-     * @throws IOException If an error occurs while reading the PEM string.
-     */
-    public PrivateKey getPrivateKey() throws IOException {
-        try (PEMParser pemParser = new PEMParser(new StringReader(appleProperty.oauth2Key()))) {
-            JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-            PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(pemParser.readObject());
-            return converter.getPrivateKey(privateKeyInfo);
-        }
     }
 
 }
--- src/main/java/com/tune_fun/v1/otp/adapter/output/persistence/OtpPersistenceAdapter.java
+++ src/main/java/com/tune_fun/v1/otp/adapter/output/persistence/OtpPersistenceAdapter.java
@@ -1,11 +1,8 @@
 package com.tune_fun.v1.otp.adapter.output.persistence;
 
 import com.tune_fun.v1.common.exception.CommonApplicationException;
+import com.tune_fun.v1.common.helper.KmsEncryptionHelper;
 import com.tune_fun.v1.common.hexagon.PersistenceAdapter;
-import com.tune_fun.v1.common.util.EncryptUtil;
-import com.tune_fun.v1.common.util.StringUtil;
-import com.tune_fun.v1.external.aws.kms.DataKey;
-import com.tune_fun.v1.external.aws.kms.KmsProvider;
 import com.tune_fun.v1.otp.application.port.output.DeleteOtpPort;
 import com.tune_fun.v1.otp.application.port.output.LoadOtpPort;
 import com.tune_fun.v1.otp.application.port.output.SaveOtpPort;
@@ -22,22 +19,14 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Objects;
 
 import static com.tune_fun.v1.common.response.MessageCode.*;
-import static com.tune_fun.v1.common.util.StringUtil.concatWithColon;
+import static com.tune_fun.v1.common.util.StringUtil.randomNumeric;
 import static com.tune_fun.v1.otp.adapter.output.persistence.OtpType.fromLabel;
 import static java.lang.String.format;
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.apache.tomcat.util.codec.binary.Base64.decodeBase64;
-import static org.apache.tomcat.util.codec.binary.Base64.encodeBase64String;
 
 
 @Component
@@ -46,7 +35,7 @@ import static org.apache.tomcat.util.codec.binary.Base64.encodeBase64String;
 public class OtpPersistenceAdapter implements SaveOtpPort, LoadOtpPort, VerifyOtpPort, DeleteOtpPort {
 
     private final RedisTemplate<String, OtpRedisEntity> redisTemplate;
-    private final KmsProvider kmsProvider;
+    private final KmsEncryptionHelper kmsEncryptionHelper;
 
     @Value("${otp.validity}")
     private Duration otpValidity;
@@ -55,7 +44,7 @@ public class OtpPersistenceAdapter implements SaveOtpPort, LoadOtpPort, VerifyOt
     public CurrentOtp saveOtp(final SaveOtp saveOtp) throws Exception {
         OtpType otpTypeConstant = getConstant(saveOtp.otpType());
         String otpKey = setOtpKey(otpTypeConstant, saveOtp.username());
-        String encryptedToken = generateEncryptedToken();
+        String encryptedToken = kmsEncryptionHelper.encrypt(randomNumeric(6));
 
         OtpRedisEntity otpRedisEntity = new OtpRedisEntity(saveOtp.username(), encryptedToken);
 
@@ -73,7 +62,7 @@ public class OtpPersistenceAdapter implements SaveOtpPort, LoadOtpPort, VerifyOt
         ValueOperations<String, OtpRedisEntity> ops = redisTemplate.opsForValue();
         OtpRedisEntity otpRedisEntity = ops.get(setOtpKey(otpTypeConstant, loadOtp.username()));
 
-        String decrypted = getDecryptedToken(otpRedisEntity);
+        String decrypted = kmsEncryptionHelper.decrypt(otpRedisEntity.getToken());
         return new CurrentDecryptedOtp(loadOtp.username(), otpTypeConstant.getLabel(), decrypted);
     }
 
@@ -106,25 +95,6 @@ public class OtpPersistenceAdapter implements SaveOtpPort, LoadOtpPort, VerifyOt
         redisTemplate.expire(otpKey, Duration.ZERO);
     }
 
-    private String generateEncryptedToken() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        String token = StringUtil.randomNumeric(6);
-        DataKey dataKey = kmsProvider.makeDataKey();
-        String encryptedToken = EncryptUtil.encrypt(dataKey.plainTextKey(), token.getBytes(UTF_8));
-
-        return concatWithColon(encodeBase64String(dataKey.encryptedKey()), encryptedToken);
-    }
-
-    private String getDecryptedToken(@NotNull OtpRedisEntity otpRedisEntity) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        String token = otpRedisEntity.getToken();
-
-        int keyEnd = token.indexOf(':');
-        byte[] encryptedKey = decodeBase64(token.substring(0, keyEnd));
-        byte[] encryptedToken = decodeBase64(token.substring(keyEnd + 1));
-        byte[] plainText = kmsProvider.requestPlaintextKey(encryptedKey).plaintext().asByteArray();
-
-        return EncryptUtil.decrypt(plainText, encryptedToken);
-    }
-
     private void setExpiredAt(String otpKey) {
         Date now = new Date();
         Date expireDate = new Date(now.getTime() + otpValidity.toMillis());
@@ -136,7 +106,7 @@ public class OtpPersistenceAdapter implements SaveOtpPort, LoadOtpPort, VerifyOt
     }
 
     private Boolean checkMatchValue(@NotNull String value, @NotNull OtpRedisEntity otpRedisEntity) throws Exception {
-        String decrypted = getDecryptedToken(otpRedisEntity);
+        String decrypted = kmsEncryptionHelper.decrypt(otpRedisEntity.getToken());
         return Objects.equals(value, decrypted);
     }
 
