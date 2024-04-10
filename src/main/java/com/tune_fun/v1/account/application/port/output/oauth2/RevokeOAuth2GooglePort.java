ceb80bf84164398598c403166b5a7bee4028299e
habin <danny9643@naver.com>
2024-04-10 17:09:37 +0900

    fix(oauth2) Revoke Revoke Http Output Port API Endpoint 수정

--- src/main/java/com/tune_fun/v1/account/application/port/output/oauth2/RevokeOAuth2GooglePort.java
+++ src/main/java/com/tune_fun/v1/account/application/port/output/oauth2/RevokeOAuth2GooglePort.java
@@ -2,6 +2,7 @@ package com.tune_fun.v1.account.application.port.output.oauth2;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(contextId = "revokeGoogleOAuth2Port", name = "revokeGoogleOAuth2Port", url = "https://oauth2.googleapis.com/revoke")
@@ -11,7 +12,7 @@ public interface RevokeOAuth2GooglePort {
      * @param accessToken Google OAuth2 access token
      * @see <a href="https://developers.google.com/identity/protocols/oauth2/web-server#tokenrevoke">Revoking (invalidating) a token</a>
      */
-    @GetMapping
+    @PostMapping
     String revokeOAuth2Google(@RequestParam("token") String accessToken);
 
 }
