170ad11aa23c0e37a299fea382732b6c5fff5d7e
habin <danny9643@naver.com>
2024-07-04 23:08:05 +0900

    feature(interaction) Search
    chore: SearchController 선언

---
+++ src/main/java/com/tune_fun/v1/interaction/adapter/input/rest/SearchController.java
@@ -0,0 +1,14 @@
+package com.tune_fun.v1.interaction.adapter.input.rest;
+
+import com.tune_fun.v1.common.response.ResponseMapper;
+import com.tune_fun.v1.common.stereotype.WebAdapter;
+import lombok.RequiredArgsConstructor;
+
+@WebAdapter
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final ResponseMapper responseMapper;
+
+
+}
--- src/main/java/com/tune_fun/v1/otp/adapter/input/rest/ResendOtpController.java
+++ src/main/java/com/tune_fun/v1/otp/adapter/input/rest/ResendOtpController.java
@@ -1,10 +1,10 @@
 package com.tune_fun.v1.otp.adapter.input.rest;
 
 import com.tune_fun.v1.common.config.Uris;
-import com.tune_fun.v1.common.stereotype.WebAdapter;
 import com.tune_fun.v1.common.response.BasePayload;
 import com.tune_fun.v1.common.response.Response;
 import com.tune_fun.v1.common.response.ResponseMapper;
+import com.tune_fun.v1.common.stereotype.WebAdapter;
 import com.tune_fun.v1.otp.application.port.input.command.OtpCommands;
 import com.tune_fun.v1.otp.application.port.input.usecase.ResendOtpUseCase;
 import jakarta.validation.Valid;
@@ -12,11 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import static com.tune_fun.v1.common.response.MessageCode.SUCCESS_OTP_RESEND;
 
-@RestController
 @WebAdapter
 @RequiredArgsConstructor
 public class ResendOtpController {
