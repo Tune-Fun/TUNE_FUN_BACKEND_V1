c870eca351a1641f33f9f7e550218c518cb4e599
twoweekhee <myjjoo4758@icloud.com>
2024-09-18 16:31:03 +0900

    fix: if null throw exception

--- src/main/java/com/tune_fun/v1/account/application/service/FindArtistService.java
+++ src/main/java/com/tune_fun/v1/account/application/service/FindArtistService.java
@@ -2,6 +2,7 @@ package com.tune_fun.v1.account.application.service;
 
 import com.tune_fun.v1.account.application.port.input.usecase.FindArtistUseCase;
 import com.tune_fun.v1.account.application.port.output.LoadAccountPort;
+import com.tune_fun.v1.common.exception.CommonApplicationException;
 import com.tune_fun.v1.common.stereotype.UseCase;
 import com.tune_fun.v1.interaction.domain.ArtistInfo;
 import com.tune_fun.v1.interaction.domain.ScrollableArtist;
@@ -19,6 +20,6 @@ public class FindArtistService implements FindArtistUseCase {
     @Override
     @Transactional(readOnly = true)
     public ArtistInfo findArtist(Long artistId) {
-        return loadAccountPort.findArtist(artistId).orElse(null);
+        return loadAccountPort.findArtist(artistId).orElseThrow(CommonApplicationException.NOT_FOUND_DATA);
     }
 }
